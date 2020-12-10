35a4f6008aebe6ab07f3982ec7eeb6773ce10343
Abdul Rauf <raufyy4@gmail.com>
2020-12-10 22:30:53 +0500

    1-introduction

--- 1-Introduction/introduction.py
+++ 1-Introduction/introduction.py
@@ -1,3 +1,18 @@
-# print on console
-print("Run Forrest Run")
-# variables
+# make sure you have installed vs code and python so we can continue
+# extensions install python
+# make sure to follow along
+
+print("Run forrest Run")
+# how to run...
+# extension way
+# vs code terminal way
+# command prompt way
+
+
+# how variables work.
+# just like a store
+name = input("Write your name: ")
+print(name + " is your name, welcome " + name)
+
+# this program inputs your name
+# print(123)
